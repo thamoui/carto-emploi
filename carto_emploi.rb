a697083a4e7809caa0469cb484fc2a7d715cc4bf
Anne-Marie Esteves <annemariee@free.fr>
2015-06-01 17:22:18 +0200

    add pagination for all offers

--- carto_emploi.rb
+++ carto_emploi.rb
@@ -18,19 +18,62 @@ end
 
 #/--------------/emploi : renvoi un tableau JSON avec tous les emplois
 #------- renvoie les 10 dernières ajoutées
-get '/emploi/:paginate' do
-    content_type :json
-  @paginate = params[:paginate]
+
+get '/test' do
+  content_type :json
   @data_job = []
-  @conn.exec("SELECT * FROM job_offers ORDER BY id_key DESC LIMIT #{@paginate}").map do |result|
+  @conn.exec("SELECT * FROM job_offers ORDER BY id_key ASC LIMIT 9 OFFSET 0").map do |result|
     result = result
     @data_job << result
   end
  @data_job.to_json
 end
 
+get '/emploi' do
+  content_type :json
+  # matches "GET /offres?paginate=1&nb_offers=10"
+  paginate = params['paginate'].to_i
+  offers = params['offers'].to_i
+
+  #----------- Counting number of all offers in database -----------------------
+  total_offers = @conn.exec("SELECT COUNT (*) FROM job_offers").map do |total_offers|
+    @total = total_offers["count"].to_i
+    puts "---------------> number of offers in db #{@total}"
+  end
+    paginate_all = (@total.to_f / offers).ceil
+    puts "---------------> this is number of pages availables for pagination #{paginate_all}"
+
+
+  if offers == 0 #si on ne met pas paginate, c egal a 0
+    limit = 10 #afficher, 10, 20 ou 500 annonces
+    bg_offers = offers
+    paginate = 0
+  else
+    #ex offers = 10 & paginate = 2
+    limit = offers #10
+    bg_offers = offers - limit
+    if paginate >= 1 && paginate <= paginate_all
+      bg_offers = offers * (paginate - 1)
+      puts "---------------> bg_offers : offers offsert startint at row n° :  #{bg_offers}"
+    end
+
+  end
+
+#------------ ORDER BY ASC : renvoie dernier enregistrements de la base, choisir plutôt la date de parution ou autre
+  @data_job = []
+  @conn.exec("SELECT * FROM job_offers ORDER BY id_key ASC LIMIT #{limit} OFFSET #{bg_offers}").map do |result|
+    puts result["id_key"]
+    @data_job << result
+
+    end
+  puts "number of elements in the @data_job hash #{@data_job.length}"
+
+  @data_job.to_json
+
+ end
+
 #--------------- /emploi/ID : renvoi l'emploi via son id
-get '/emploi/:id' do
+get '/offres/:id' do
   id = ''
   id << params[:id]
   puts "---------------this is id #{id}"
