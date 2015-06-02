3b58a301e933a0ad4d098a027fba574635c799c8
Anne-Marie Esteves <annemariee@free.fr>
2015-06-02 17:19:44 +0200

    refactoring search engine for jobs and pagination

--- carto_emploi.rb
+++ carto_emploi.rb
@@ -15,53 +15,6 @@ end
 get '/' do
   send_file 'views/index.html'
 end
-
-#/--------------/emploi : renvoi un tableau JSON avec tous les emplois
-#------- renvoie les 10 dernières ajoutées
-
-get '/emploi' do
-  # matches "GET /offres?paginate=1&nb_offers=10"
-  content_type :json
-  paginate = params['paginate'].to_i
-  offers = params['offers'].to_i
-
-  #----------- Counting number of all offers in database -----------------------
-  total_offers = @conn.exec("SELECT COUNT (*) FROM job_offers").map do |total_offers|
-    @total = total_offers["count"].to_i
-    puts "---------------> number of offers in db #{@total}"
-  end
-
-  if offers == 0 #afficher, 10, 20 ou 50 annonces, nombre qui bouge suivant le nbre d'offers disponibles dans la BDD
-    limit = 10 #afficher, 10, 20 ou 50 annonces, nombre fixe
-    bg_offers = offers
-    paginate = 0
-    all_pages = (@total.to_f / limit).ceil
-    puts "---------------> this is nb of pages availables for pagination :  #{all_pages} // if offers == 0"
-  else
-    limit = offers
-    all_pages = (@total.to_f / limit).ceil
-    puts "---------------> this is number of pages availables for pagination #{all_pages} // if offers >0"
-    puts "---------- this is limit if offers =! 0 & paginate >=1 #{limit}"
-    bg_offers = offers - limit
-    if paginate >= 1 && paginate <= all_pages
-      bg_offers = offers * (paginate - 1)
-      puts "---------------> paginate: offers for page n° :  #{paginate}"
-      puts "---------------> limit: nb of offers per pages :  #{limit}"
-
-      puts "---------------> bg_offers : offers offsert startint at row n° :  #{bg_offers}"
-    end
-  end
-
-#------------ ORDER BY ASC : renvoie les premiers enregistrements de la base par id_key, choisir plutôt la date de parution ou autre
-  @data_job = []
-  @conn.exec("SELECT * FROM job_offers ORDER BY id_key ASC LIMIT #{limit} OFFSET #{bg_offers}").map do |result|
-    puts result["id_key"]
-    @data_job << result
-    end
-  puts "number of elements in the @data_job hash #{@data_job.length}"
-  @data_job.to_json
- end
-
 #--------------- /emploi/ID : renvoi l'emploi via son id
 get '/emploi/:id' do
   content_type :json
@@ -74,19 +27,104 @@ get '/emploi/:id' do
   end
   @data_job.to_json
 end
+#/--------------/emploi : renvoi un tableau JSON avec tous les emplois
+#------- renvoie les 10 dernières ajoutées
+get '/emploi' do
+  # matches "GET /emploi?p=1&limit=10"
+  content_type :json
+  page = params['p'].to_i
+  limit_given = params['limit'].to_i
+
+
+  #////////////////////////////// PAGINATION ///////////////////////
+  #----------- Counting number of all offers in database -----------------------
+  total_offers = @conn.exec("SELECT COUNT (*) FROM job_offers").map do |total_offers|
+    @total = total_offers["count"].to_i
+    puts "---------------> number of offers in db #{@total}"
+  end
+
+  if limit_given == 0 #afficher, 10, 20 ou 50 annonces, nombre qui bouge suivant le nbre d'offers disponibles dans la BDD
+    limit = 10 #afficher, 10, 20 ou 50 annonces, nombre fixe
+    bg_offers = limit_given
+    page = 0
+    all_pages = (@total.to_f / limit).ceil
+    puts "---------------> this is nb of pages availables for pagination :  #{all_pages} // if offers == 0"
+  else
+    limit = limit_given
+    all_pages = (@total.to_f / limit).ceil
+    puts "---------------> this is number of pages availables for pagination #{all_pages} // if offers >0"
+    puts "---------- this is limit if offers =! 0 & page >=1 #{limit}"
+    bg_offers = limit_given - limit
+    if page >= 1 && page <= all_pages
+      bg_offers = limit_given * (page - 1)
+      puts "---------------> page: offers for page n° :  #{page}"
+      puts "---------------> limit: nb of offers per pages, 10, 20, 50, 100 :  #{limit_given}"
+
+      puts "---------------> bg_offers : offers offsert startint at row n° :  #{bg_offers}"
+    end
+  end
+  #///////////////////////////// ENF OF PAGINATION ////////////////////////
+
+#------------ ORDER BY ASC : renvoie les premiers enregistrements de la base par id_key, choisir plutôt la date de parution ou autre
+  @data_job = []
+  @conn.exec("SELECT * FROM job_offers ORDER BY id_key ASC LIMIT #{limit} OFFSET #{bg_offers}").map do |result|
+    puts result["id_key"]
+    @data_job << result
+    end
+  puts "number of elements in the @data_job hash #{@data_job.length}"
+  @data_job.to_json
+ end
 
 #--------------   /search/S : renvoi un tableau JSON avec les emplois correspondant à la recherche
 get '/search/:text' do
   content_type :json
-  offer = ''
-  offer << params[:text]
-  puts "---------------this is job searched #{offer}"
-  @data_job = []
+  #offer = ''
+  job = params[:text]
+  page = params['p'].to_i
+  limit_given = params['limit'].to_i
 
-  result = @conn.exec("SELECT * FROM job_offers WHERE to_tsvector('french', offer_description || ' ' || title) @@ plainto_tsquery('french', '#{offer}'); ")
+
+  puts "---------------- NEW SEARCH -------------------------"
+  puts "---------------- LIMIT GIVEN : #{limit_given} -------------------------"
+
+  #////////////////////////////// PAGINATION ///////////////////////
+
+
+  #----------- Counting number of all offers in database -----------------------
+  total_offers = @conn.exec("SELECT COUNT (*) FROM job_offers").map do |total_offers|
+    @total = total_offers["count"].to_i
+    puts "---------------> number of offers in db #{@total}"
+  end # cette partie peut ne pas être copié ???
+
+  if limit_given == 0 #afficher, 10, 20 ou 50 annonces, bouge suivant le nbre d'offers disponibles dans la BDD
+    limit = 10 #afficher, 10, 20 ou 50 annonces, nombre fixe
+    bg_offers = limit_given
+    page = 0
+    all_pages = (@total.to_f / limit).ceil
+    puts "---------------> case limit==0 : this is nb of pages availables for pagination :  #{all_pages}"
+  else
+    limit = limit_given
+    all_pages = (@total.to_f / limit).ceil
+    puts "---------------> case limit >0 : #{all_pages} pages availables for pagination"
+    bg_offers = limit_given - limit
+    puts "------------------BG_OFFER #{bg_offers}"
+    if page >= 1 && page <= all_pages
+      bg_offers = limit_given * (page - 1)
+      puts "---------------> page: offers for page n° :  #{page}"
+
+      puts "---------------> limit: nb of offers per pages, 10, 20, 50, 100 :  #{limit_given}"
+
+      puts "---------------> bg_offers : offers offsert startint at row n° :  #{bg_offers}"
+    end
+  end
+  #///////////////////////////// ENF OF PAGINATION ////////////////////////
+
+  @data_job = [] #faut-il définir ça au début du fichier ? Est commun à chaque méthode
+
+  result = @conn.exec("SELECT * FROM job_offers WHERE to_tsvector('french', offer_description || ' ' || title) @@ plainto_tsquery('french', '#{job}') ORDER BY id_key ASC LIMIT #{limit} OFFSET #{bg_offers}; ")
 
     result.map do |result|
-    puts result["title"]
+    puts "---- #{result["id_key"]} // #{result["offer_id"]} : #{result["title"]}"
     @data_job << result
 
   end
@@ -100,26 +138,72 @@ end
 #--------------   /geosearch/LAT,LNG : renvoi les emplois aux alentours
 
 get '/geosearch/:lat,:lng' do
-  "renvoi les emplois aux alentours"
-  content_type :json
-
-
-  @lat = params[:lat]
-  @lng = params[:lng]
-  @distance = 5
-  @data_job = []
-
-  puts @lat
-  puts @lng
+  #geosearch/48.86833,2.66833?p=42&limit=42&text=développeur
 
   #TESTER AVEC CES VALEURS POUR EVRY
   # @lat = 48.629828
   # @lng = 2.441782
 
-  result = @conn.exec("select id_key, distance from (select id_key, ( 6371 * acos( cos( radians( #{@lat} ) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(#{@lng}) ) + sin( radians(#{@lat}) ) * sin( radians( latitude ) ) ) ) as distance from job_offers ) as dt where distance < #{@distance};")
-result.map do |result|
-  puts "-----this is result #{result}"
-    @data_job << result
+  content_type :json
+
+
+  @lat = params[:lat]
+  @lng = params[:lng]
+  @distance = 25
+  job = params['text']
+
+  page = params['p'].to_i
+  limit_given = params['limit'].to_i
+
+  @data_job = []
+
+  puts @lat
+  puts @lng
+
+  puts "---------------- NEW SEARCH -------------------------"
+  puts "---------------- LIMIT GIVEN : #{limit_given} -------------------------"
+
+  #////////////////////////////// PAGINATION ///////////////////////
+
+
+  #----------- Counting number of all offers in database -----------------------
+  total_offers = @conn.exec("SELECT COUNT (*) FROM job_offers").map do |total_offers|
+    @total = total_offers["count"].to_i
+    puts "---------------> number of offers in db #{@total}"
+  end # cette partie peut ne pas être copié ???
+
+  if limit_given == 0 #afficher, 10, 20 ou 50 annonces, bouge suivant le nbre d'offers disponibles dans la BDD
+    limit = 10 #afficher, 10, 20 ou 50 annonces, nombre fixe
+    bg_offers = limit_given
+    page = 0
+    all_pages = (@total.to_f / limit).ceil
+    puts "---------------> case limit==0 : this is nb of pages availables for pagination :  #{all_pages}"
+  else
+    limit = limit_given
+    all_pages = (@total.to_f / limit).ceil
+    puts "---------------> case limit >0 : #{all_pages} pages availables for pagination"
+    bg_offers = limit_given - limit
+    puts "------------------BG_OFFER #{bg_offers}"
+    if page >= 1 && page <= all_pages
+      bg_offers = limit_given * (page - 1)
+      puts "---------------> page: offers for page n° :  #{page}"
+
+      puts "---------------> limit: nb of offers per pages, 10, 20, 50, 100 :  #{limit_given}"
+
+      puts "---------------> bg_offers : offers offsert startint at row n° :  #{bg_offers}"
+    end
+  end
+  #///////////////////////////// ENF OF PAGINATION ////////////////////////
+
+
+
+  # result = @conn.exec("select id_key, distance from (select id_key, ( 6371 * acos( cos( radians( #{@lat} ) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(#{@lng}) ) + sin( radians(#{@lat}) ) * sin( radians( latitude ) ) ) ) as distance from job_offers ) as dt where distance < #{@distance};")
+
+
+  result = @conn.exec("SELECT *, distance FROM (SELECT *, ( 6371 * acos( cos( radians( #{@lat} ) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(#{@lng}) ) + sin( radians(#{@lat}) ) * sin( radians( latitude ) ) ) ) AS distance FROM job_offers ) AS dt WHERE distance < #{@distance} AND to_tsvector('french', offer_description || ' ' || title) @@ plainto_tsquery('french', '#{job}') ORDER BY publication_date ASC LIMIT #{limit} OFFSET #{bg_offers} ;")
+  result.map do |result|
+  puts "---- #{result["id_key"]} // #{result["offer_id"]} : #{result["title"]}"
+  @data_job << result
 
     if @data_job == []
        "Aucun emploi ne correspond à votre recherche"
