6c938bb93dd5e15615cafeb369938001371546de
Anne-Marie Esteves <annemariee@free.fr>
2015-06-30 13:03:45 +0200

    refactoring and cleaning code

--- parser/body_parser.rb
+++ parser/body_parser.rb
@@ -7,12 +7,6 @@ require 'nokogiri'
 
 class BodyParser
 
-  # attr_reader :url, :doc
-  # def initialize(url)
-  #   @url = url
-  #   @doc = Nokogiri::HTML(open(:url))
-  # end
-
   def get_body(url)
     url = "https://simplon2015exercices.herokuapp.com/"
     uri = URI.parse(url)
@@ -20,37 +14,22 @@ class BodyParser
     puts res.read_body
   end
 
-  def get_html_source(url)
-    doc = Nokogiri::HTML(open(url))
+  def get_source(url)
+    Nokogiri::HTML(open(url))
   end
-
 
   #----------- PARSING METHODS ------------------------------
 
   # ----------------L'offre n'est plus disponible -----------
   def offer_unavailable(url)
-    doc = Nokogiri::HTML(open(url))
-    doc.css('p[@class="first paragraph-embed"]').children.inner_text == "L'offre que vous souhaitez consulter n'est plus disponible."
+    get_source(url).css('p[@class="first paragraph-embed"]').children.inner_text == "L'offre que vous souhaitez consulter n'est plus disponible."
     #return true si l'offre n'est plus disponible
   end
 
   #------------------- Adresse ------------------------------
   def search_region(url)
-    # doc = Nokogiri::HTML(open(url))
-    # #je pourrais enlever region_adress de partout non ?
-    # region_adress = doc.css('li[@itemprop="addressRegion"]').children.inner_text
-    # if region_adress != nil || region_adress != ""
-    #   region_adress.gsub(/'/, "''")      # rajouter france pour que l'adresse soit mieux interprétée ?
-    #   #region_adress.sub! /^\w+\s-\s/, '' #renvoir Paris si adress est 75 - Paris
-    #   # rajouter france pour que l'adresse soit mieux interprétée ?
-    #   # region_adress + ", France"
-    # else
-    #   "Information non disponible"
-    # end
-    #
-    doc = Nokogiri::HTML(open(url))
     #je pourrais enlever region_adress de partout non ?
-    region_adress = doc.css('li[@itemprop="addressRegion"]').children.inner_text
+    region_adress = get_source(url).css('li[@itemprop="addressRegion"]').children.inner_text
     if region_adress != nil || region_adress != ""
       #region_adress.gsub(/'/, "''")
       ["PARIS", "LYON", "MARSEILLE"].each do |city|
@@ -77,8 +56,7 @@ class BodyParser
 
   #------------------- Ville------------------------------
   def search_city(url)
-    doc = Nokogiri::HTML(open(url))
-    city = doc.css('li[@itemprop="addressRegion"]').children.inner_text
+    city = get_source(url).css('li[@itemprop="addressRegion"]').children.inner_text
     if  (city =~ /[0-9](.*)/) == nil
       city
     elsif city != nil || city != ""
@@ -94,8 +72,7 @@ class BodyParser
 
   #------------------- Département ------------------------------
   def search_dept(url)
-    doc = Nokogiri::HTML(open(url))
-    dpt = doc.css('li[@itemprop="addressRegion"]').children.inner_text
+    dpt = get_source(url).css('li[@itemprop="addressRegion"]').children.inner_text
     # attention le lieu de travail ne contient pas de numéro de département parfois comme France ou Pays de la loire
     if dpt != nil || region_adress != ""
       dpt = dpt[/[^ -]+/]
@@ -106,8 +83,7 @@ class BodyParser
 
   #--------- Intitulé du poste ---------
   def search_title(url)
-    doc = Nokogiri::HTML(open(url))
-    job_title = doc.css('h4[@itemprop="title"]').children.inner_text
+    job_title = get_source(url).css('h4[@itemprop="title"]').children.inner_text
     if job_title != nil
       job_title.gsub(/'/, "''")
     else
@@ -117,8 +93,7 @@ class BodyParser
 
   #--------- Type de contrat ---------
   def search_employment_type(url)
-    doc = Nokogiri::HTML(open(url))
-    employement_type = doc.css('span[@itemprop="employmentType"]').children.inner_text
+    employement_type = get_source(url).css('span[@itemprop="employmentType"]').children.inner_text
     if   employement_type != nil || employement_type != "" || employement_type.string?
       employement_type[/[^-]+/].strip
     else
@@ -128,8 +103,7 @@ class BodyParser
 
   #--------- Code Rome ---------
   def search_code_rome(url)
-    doc = Nokogiri::HTML(open(url))
-    code_rome = doc.css('p[@itemprop="occupationalCategory"]').children.inner_text
+    code_rome = get_source(url).css('p[@itemprop="occupationalCategory"]').children.inner_text
     if code_rome != nil || code_rome != ""
       code_rome.gsub!(/Métier du ROME /, "")[0..4]
     else
@@ -139,8 +113,7 @@ class BodyParser
 
   #------------ Vérifie si code rome est bien dans la liste des métiers
   def check_code_rome(url)
-    doc = Nokogiri::HTML(open(url))
-    code_rome = doc.css('p[@itemprop="occupationalCategory"]').children.inner_text
+    code_rome = get_source(url).css('p[@itemprop="occupationalCategory"]').children.inner_text
     if code_rome != nil || code_rome != ""
       code_rome = code_rome.gsub(/Métier du ROME /, "")[0..4]
       code_rome_info = ["M1801", "M1802", "M1803", "M1804", "M1805", "M1806", "M1810", "I1401", "H1208", "E1101", "E1104", "E1205", "E1402"]
@@ -152,18 +125,15 @@ class BodyParser
   end
   #----------- Date de publication -------
   def search_publication_date(url)
-    doc = Nokogiri::HTML(open(url))
-    publication_date = doc.css('span[@itemprop="datePosted"]').children.inner_text
+    get_source(url).css('span[@itemprop="datePosted"]').children.inner_text
   end
 
   #----------- Description de l'offre -----------
   def search_description_offer(url)
-    doc = Nokogiri::HTML(open(url))
-    description_offer = doc.css('p[@itemprop="description"]').inner_html
+    description_offer = get_source(url).css('p[@itemprop="description"]').inner_html
     description_offer.strip
     if description_offer != nil || description_offer != ""
       description_offer.gsub(/'/, "''")
-
     else
       description_offer =  "Information non disponible"
     end
@@ -171,8 +141,7 @@ class BodyParser
 
   # ----------- Nom de l'entreprise -------------
   def search_company_description(url)
-    doc = Nokogiri::HTML(open(url))
-    company_description = doc.xpath("//div[contains(@class,'vcard')]/p/text()").collect {|node| node.text}
+    company_description = get_source(url).xpath("//div[contains(@class,'vcard')]/p/text()").collect {|node| node.text}
     company_description[0]
     if company_description[0] != nil #|| company_description[0] != ""
       company_description[0].gsub(/'/, "''").strip
--- test/body_parser_test.rb
+++ test/body_parser_test.rb
@@ -6,20 +6,12 @@ require File.expand_path '../test_helper.rb', __FILE__
 
 class BodyParserTest < Minitest::Test
 
-
-  # def initialize(url)
-  #   @url = url
-  # end
-    # def get_body(url)
-    #   url = 'http://0.0.0.0:8000/test/_027FLJF.html' #apache must be started
-    # end
-
   #Aller voir ce github pour voir comment refactorer ses tests : https://github.com/seattlerb/minitest
 
   print "Start a server with $ruby -run -e httpd . -p 8000"
 
   def doc
-   @newdoc = ::BodyParser.new()
+    ::BodyParser.new
   end
 
   def test_1_find_region
@@ -50,32 +42,32 @@ class BodyParserTest < Minitest::Test
     assert_equal expect, doc.search_code_rome(url)
   end
 
-  def test_4_find_publication_code
-    @url = "http://0.0.0.0:8000/test/offre_test_027FLJF.html"
+  def test_4_find_publication_date
+    url = "http://0.0.0.0:8000/test/offre_test_027FLJF.html"
     expect ="11/05/2015"
-    assert_equal expect, doc.search_publication_date(@url)
+    assert_equal expect, doc.search_publication_date(url)
   end
 
   def test_5_Job_Offer_Description
-    @url = "http://0.0.0.0:8000/test/offre_test_027FLJF.html"
+    url = "http://0.0.0.0:8000/test/offre_test_027FLJF.html"
     expect = "En premier lieu joindre des vraies  références  de site réalisés à votre  Cv <br><br>Vous devrez élaborer des stratégies digitales : audits des divers sites du groupe<br>- benchmark, identification des best-practices et rédaction des recommandations<br>Suivi opérationnel du déploiement <br>Webmastering, Community Management<br>Suivi et mesure de la performance des dispositifs mis en place<br>Serez en charge de l''optimisation SEO/SEA, comparateurs, et rédaction web (suivi et développement du référencement naturel, création et optimisation  adwords, remarketing<br>Vous aurez en charge le développement du référencement et de son optimisation.<br>Vous piloterez les données des conversions, analyserez le trafic, réaliserez des tests de type AB, et gèrerez les newsletters.<br>Vous pourrez également intervenir sur le CSS ou le design du site, et effectuer des mises à jours (fiches produits....)<br>Connaissances PHP OBjet CSS, html, et Photoshop seraient un reel plus<br>CONNAISSANCE EN DESIGN IMPERATIF"
-    assert_equal expect, doc.search_description_offer(@url)
+    assert_equal expect, doc.search_description_offer(url)
   end
 
   def test_6_Company_Description
-    @url = "http://0.0.0.0:8000/test/offre_test_027FLJF.html"
+    url = "http://0.0.0.0:8000/test/offre_test_027FLJF.html"
     expect = "INFO MAX"
-    assert_equal expect, doc.search_company_description(@url)
+    assert_equal expect, doc.search_company_description(url)
   end
 
   def test_7_Offre_Non_Disponible
-    @url = "http://0.0.0.0:8000/test/offre_non_disponible.html"
-    assert_equal true, doc.offer_unavailable(@url)
+    url = "http://0.0.0.0:8000/test/offre_non_disponible.html"
+    assert_equal true, doc.offer_unavailable(url)
   end
 
   def test_8_Paris_dep_devient_ville
-    @url = "http://0.0.0.0:8000/test/offre_test_paris_dpt.html"
+    url = "http://0.0.0.0:8000/test/offre_test_paris_dpt.html"
     expect = "75 , PARIS , FRANCE"
-    assert_equal expect, doc.search_region(@url)
+    assert_equal expect, doc.search_region(url)
   end
 end
