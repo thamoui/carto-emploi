1b2f5d105631fa68da9238c949240c65be4ef4c7
Anne-Marie Esteves <annemariee@free.fr>
2015-07-01 11:07:21 +0200

    sql table job list got new job

--- db/structure.sql
+++ db/structure.sql
@@ -5,21 +5,22 @@ CREATE TABLE job_list (
   code_rome text
   );
 
-INSERT INTO job_list (id_key, slug, label, code_rome) VALUES
-(1, 'Administrateur', 'Administrateur', 'M1801'),
-(2, 'Administrateur de base de données', 'Administrateur de base de données', 'M1801'),
-(3, 'Chef de projet web', 'Chef de projet web', 'M1803'),
-(4, 'Développeur', 'Développeur', 'M1805'),
-(5, 'Ingénieur informatique', 'Ingénieur informatique', 'M1810'),
-(6, 'Intégrateur', 'Intégrateur', 'M1805'),
-(7, 'Sécurité informatique', 'Sécurité informatique', 'M1801'),
-(8, 'Webmaster', 'Webmaster', 'M1805'),
-(9, 'Informaticien', 'Informaticien', 'M1805'),
-(10, 'Informatique', 'Informatique', 'M1805'),
-(11, 'Architecte', 'Architecte', 'M1802'),
-(12, 'Responsable informatique', 'Responsable informatique', 'M1803'),
-(13, 'Testeur informatique', 'Testeur informatique', 'M1805')
-;
+  INSERT INTO job_list (id_key, slug, label, code_rome) VALUES
+  (1, '', 'Tous métiers informatique', ''),
+  (2, 'Administrateur', 'Administrateur', 'M1801'),
+  (3, 'Administrateur de base de données', 'Administrateur de base de données', 'M1801'),
+  (4, 'Chef de projet web', 'Chef de projet web', 'M1803'),
+  (5, 'Développeur', 'Développeur', 'M1805'),
+  (6, 'Ingénieur informatique', 'Ingénieur informatique', 'M1810'),
+  (7, 'Intégrateur', 'Intégrateur', 'M1805'),
+  (8, 'Sécurité informatique', 'Sécurité informatique', 'M1801'),
+  (9, 'Webmaster', 'Webmaster', 'M1805'),
+  (10, 'Informaticien', 'Informaticien', 'M1805'),
+  (11, 'Informatique', 'Informatique', 'M1805'),
+  (12, 'Architecte', 'Architecte', 'M1802'),
+  (13, 'Responsable informatique', 'Responsable informatique', 'M1803'),
+  (14, 'Testeur informatique', 'Testeur informatique', 'M1805')
+  ;
 
 CREATE TABLE job_offers (
   region_adress text,
