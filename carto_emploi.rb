b6ade1440b9b65ffbaff34296669c8a89915fe58
Anne-Marie Esteves <annemariee@free.fr>
2015-07-29 11:40:05 +0200

    add configuration for dokku digital ocean

--- carto_emploi.rb
+++ carto_emploi.rb
@@ -30,9 +30,8 @@ ActiveRecord::Base.default_timezone = :utc
 
 #----------------------- DB CONFIG  ---------------------------
 if ENV['RACK_ENV'] == "production"
-  db_parts = ENV['DATABASE_URL'].split(/\/|:|@/)
   configure do
-    set :conn, PG.connect(host: db_parts[5], port: 5432, dbname: db_parts[7], user: db_parts[3], password: db_parts[4])
+    set :conn, PG.connect(host: "127.0.0.1", port: 5432, dbname: ENV['DATABASE_NAME'], user: ENV['DATABASE_USER_NAME'], password: ENV['DATABASE_PASSWORD'])
   end
 else
   require 'shotgun'
