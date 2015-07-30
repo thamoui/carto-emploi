5dc6282827fb7dc9321388515085769a1b6fb4d3
Anne-Marie Esteves <annemariee@free.fr>
2015-07-30 13:19:58 +0200

    use CONN var instead of conn

--- parser/delete_unavailable_job_offers.rb
+++ parser/delete_unavailable_job_offers.rb
@@ -6,10 +6,10 @@ require_relative 'body_parser'
 # #----------------------- HEROKU DB CONFIG  ------------------------
 # if ENV['RACK_ENV'] == "production"
 #   db_parts = ENV['DATABASE_URL'].split(/\/|:|@/)
-#   conn = PGconn.connect(host: db_parts[5], port: 5432, dbname: db_parts[7], user: db_parts[3], password: db_parts[4])
+#   CONN = PGconn.connect(host: db_parts[5], port: 5432, dbname: db_parts[7], user: db_parts[3], password: db_parts[4])
 # else
 #   #----------------------- CONNECT DATABASE LOCALHOST ----------------------
-#   conn = PGconn.connect(host: "127.0.0.1", port: 5432, dbname: ENV['DATABASE_NAME'], user: ENV['DATABASE_USER_NAME'], password: ENV['DATABASE_PASSWORD'])
+#   CONN = PGconn.connect(host: "127.0.0.1", port: 5432, dbname: ENV['DATABASE_NAME'], user: ENV['DATABASE_USER_NAME'], password: ENV['DATABASE_PASSWORD'])
 #   require 'colorize'
 # end
 
@@ -21,7 +21,7 @@ def doc
 end
 
 #---------------- GETTING AN ARRAY OF URLS FROM TABLE JOB OFFERS ---------------------------------
-@result = conn.exec( "SELECT url FROM job_offers").to_a
+@result = CONN.exec( "SELECT url FROM job_offers").to_a
 puts @result[0]
 puts "------------------->>> THERE IS #{@result.length} URLS IN ARRAY <<<------------------------"
 nb_offres = @result.length #décompte de ce qu'il reste à insérer ^^
@@ -34,7 +34,7 @@ deleted_offer = 0
   puts "_________________________________________________________________________________"
   puts "_________________ STARTING CLEANING JOB OFFERS BASE _____________________________"
   if doc.offer_unavailable(item["url"]) == true
-    conn.exec("DELETE FROM job_offers WHERE url = '#{item["url"]}'")
+    CONN.exec("DELETE FROM job_offers WHERE url = '#{item["url"]}'")
     deleted_offer = deleted_offer + 1
     puts "-------- L'url #{item["url"]} a été supprimé de la bdd job_offers-------- "
   end
