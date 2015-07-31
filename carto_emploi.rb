bdc1395ef27da2144558d0c0a4de800efbf61d33
Anne-Marie Esteves <annemariee@free.fr>
2015-07-31 15:53:57 +0200

    clean comments and disable new_relic too

--- carto_emploi.rb
+++ carto_emploi.rb
@@ -20,9 +20,9 @@ end
 
 
 #add new relic in addons for Software Analytics, Application Performance Mangement
-configure :production do
-  require 'newrelic_rpm'
-end
+# configure :production do
+#   require 'newrelic_rpm'
+# end
 
 #------------------------ config.time_zone = 'Europe/Paris'-----
 Time.zone = "UTC"
@@ -30,40 +30,26 @@ ActiveRecord::Base.default_timezone = :utc
 
 # ----------------------- DB CONFIG  ---------------------------
 if ENV['RACK_ENV'] == "production"
-   db_parts = ENV['DATABASE_URL'].split(/\/|:|@/)
-   #configure do
-     CONN = PG::Connection.new({host: db_parts[5], port: db_parts[6], dbname: db_parts[7], user: db_parts[3], password: db_parts[4]})
-   #end
- else
-   require 'shotgun'
-   #configure do
-    CONN = PG::Connection.new({host: "127.0.0.1", port: 5432, dbname: ENV['DATABASE_NAME'], user: ENV['DATABASE_USER_NAME'], password: ENV['DATABASE_PASSWORD']})
-#   end
-  end
-
-# CONN = PG.connect(host: "127.0.0.1", port: 5432, dbname: ENV['DATABASE_NAME'], user: ENV['DATABASE_USER_NAME'], password: ENV['DATABASE_PASSWORD'])
-#
-# puts "this is #{CONN.inspect}"
-
-
-
-#------------- Methode qui checke avant une route si la connection est valide ------
-def check_connection( conn )
-  begin
-    CONN.exec("SELECT 1")
-  rescue PG::Error => err
-    $stderr.puts "%p while CHECKING TESTING connection: %s" % [ err.class, err.message ]
-    CONN.reset
-    puts "--------- PG CONNECTION RESETED -------------"
-  end
+  db_parts = ENV['DATABASE_URL'].split(/\/|:|@/)
+  #configure do
+  CONN = PG::Connection.new({host: db_parts[5], port: db_parts[6], dbname: db_parts[7], user: db_parts[3], password: db_parts[4]})
+  #end
+else
+  require 'shotgun'
+  #configure do
+  CONN = PG::Connection.new({host: "127.0.0.1", port: 5432, dbname: ENV['DATABASE_NAME'], user: ENV['DATABASE_USER_NAME'], password: ENV['DATABASE_PASSWORD']})
+  #   end
 end
 
-# before do
-# #   CONN = settings.conn
-#
-# CONN = PG.connect(host: "127.0.0.1", port: 5432, dbname: ENV['DATABASE_NAME'], user: ENV['DATABASE_USER_NAME'], password: ENV['DATABASE_PASSWORD'])
-#
-# puts "this is CONN in before do #{CONN.inspect}"
+# #------------- Methode qui checke avant une route si la connection est valide ------
+# def check_connection( conn )
+#   begin
+#     CONN.exec("SELECT 1")
+#   rescue PG::Error => err
+#     $stderr.puts "%p while CHECKING TESTING connection: %s" % [ err.class, err.message ]
+#     CONN.reset
+#     puts "--------- PG CONNECTION RESETED -------------"
+#   end
 # end
 
 set :public_folder, 'frontend' #this is necessary to be able to access to static files
@@ -92,7 +78,7 @@ get '/geosearch/:lat,:lng' do
   puts "--------- GEOSEARCH CONN CLASS : #{CONN.class}"
 
   #------------ Checking if there is a connection do database
-  check_connection(CONN)
+  # check_connection(CONN)
   #------------ end checking
 
   content_type :json, 'charset' => 'utf-8'
@@ -113,21 +99,19 @@ get '/geosearch/:lat,:lng' do
   #////////////////////////////// PAGINATION ///////////////////////
   #----------- Counting number of all offers in database -----------------------
   total_offers = CONN.exec("SELECT COUNT (id_key) FROM job_offers")
-    @total = total_offers[0]["count"].to_i
-    puts "---------------> number of offers in db #{@total}"
+  @total = total_offers[0]["count"].to_i
+
+  puts "---------------> number of offers in db #{@total}"
 
   if limit_given == 0 #afficher, 10, 20 ou 50 annonces, bouge suivant le nbre d'offers disponibles dans la BDD
     limit = 100 #afficher, 10, 20 ou 50 annonces, nombre fixe
     bg_offers = limit_given
     page = 0
     all_pages = (@total.to_f / limit).ceil
-    puts "---------------> case limit==0 : this is nb of pages availables for pagination :  #{all_pages}"
   else
     limit = limit_given
     all_pages = (@total.to_f / limit).ceil
-    puts "---------------> case limit >0 : #{all_pages} pages availables for pagination"
     bg_offers = limit_given - limit
-    puts "------------------BG_OFFER #{bg_offers}"
     if page >= 1 && page <= all_pages
       bg_offers = limit_given * (page - 1)
     end
@@ -140,26 +124,21 @@ get '/geosearch/:lat,:lng' do
     #sql = "AND to_tsvector('french', offer_description || ' ' || title) @@ plainto_tsquery('french', '#{job}')"
     sql = "AND title LIKE '%#{job}%'"
   end
+
   requete_sql = "SELECT *, distance FROM (SELECT *, ( 6371 * acos( cos( radians( #{@lat} ) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(#{@lng}) ) + sin( radians(#{@lat}) ) * sin( radians( latitude ) ) ) ) AS distance FROM job_offers ) AS dt WHERE distance < #{@distance} #{sql} ORDER BY publication_date DESC LIMIT #{limit} OFFSET #{bg_offers} ;"
   result = CONN.exec(requete_sql)
-
-  puts "------------- this is RESULT #{result.inspect}-----------"
 
   result.map do |data|
     puts "---- #{data["publication_date"]}  // //  #{data["region_adress"]} //  #{data["id_key"]} // #{data["offer_id"]} : #{data["title"]}"
     @data_job << data
   end
 
-  if @data_job == [] #an empty data is analysed by index.html
-    [].to_json
-  else
-    @data_job.to_json
-  end
+  @data_job.to_json
+
+  # if @data_job == [] #an empty data is analysed by index.html
+  #   [].to_json
+  # else
+  #   @data_job.to_json
+  # end
+
 end
-
-
-# # ------------- Message d'erreur personnalisé
-# error do
-#    @error = env['sinatra.error']
-#    erb :err500, :locals => {:error => error}
-#  end
