36d0abcb03ed79fc2949090fe3dffd2915425151
annemarie35 <annemariee@free.fr>
2016-01-27 11:50:37 +0100

    Modification des intitulés de métier pour corriger rapidement le bug du à la modification par pole emploi de la structure des urls

--- db/create_lang_table.sql
+++ db/create_lang_table.sql
@@ -5,20 +5,19 @@ CREATE TABLE lang_lists (
   popularity number
   );
 
-  INSERT INTO job_lists (id_key, slug, label, code_rome) VALUES
-  
-  (1, 'javaScript', 'javaScript', ''),
+ INSERT INTO job_lists (id_key, slug, label, code_rome) VALUES
+  (1, '', 'Tous métiers informatique', ''),
   (2, 'Administrateur', 'Administrateur', 'M1801'),
   (3, 'Administrateur de base de données', 'Administrateur de base de données', 'M1801'),
-  (4, 'Chef de projet web', 'Chef de projet web', 'M1803'),
-  (5, 'Développeur', 'Développeur', 'M1805'),
-  (6, 'Ingénieur informatique', 'Ingénieur informatique', 'M1810'),
-  (7, 'Intégrateur', 'Intégrateur', 'M1805'),
-  (8, 'Sécurité informatique', 'Sécurité informatique', 'M1801'),
+  (4, 'Chef de projet web', 'Chef$0020de$0020projet$0020web', 'M1803'),
+  (5, 'Développeur', 'D$00e9veloppeur', 'M1805'),
+  (6, 'Ingénieur informatique', 'Ing$00e9nieur$0020informatique', 'M1810'),
+  (7, 'Intégrateur', 'Int$00e9grateur', 'M1805'),
+  (8, 'Sécurité informatique', 'S$00e9curit$00e9$0020informatique', 'M1801'),
   (9, 'Webmaster', 'Webmaster', 'M1805'),
   (10, 'Informaticien', 'Informaticien', 'M1805'),
   (11, 'Informatique', 'Informatique', 'M1805'),
   (12, 'Architecte', 'Architecte', 'M1802'),
-  (13, 'Responsable informatique', 'Responsable informatique', 'M1803'),
-  (14, 'Testeur informatique', 'Testeur informatique', 'M1805')
+  (13, 'Responsable informatique', 'Responsable$0020informatique', 'M1803'),
+  (14, 'Testeur informatique', 'Testeur$0020informatique', 'M1805')
   ;
